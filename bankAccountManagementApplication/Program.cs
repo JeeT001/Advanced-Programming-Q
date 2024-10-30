adacb8770b3e15e0f323739199dedb1a3dfc6cf6
Simranjit Babbar <111028218+JeeT001@users.noreply.github.com>
2024-10-30 14:38:41 +1300

    Project creation
    Created the project with customer class, not completed yet.

---
+++ bankAccountManagementApplication/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace bankAccountManagementApplication
+{
+
+
+    public class Customer
+    {
+        private int customerId { get; set; }
+        private int customerNumber { get; set; }
+
+        private string customerAddress { get; set; }
+
+        public string customerName;
+
+        public Customer(int id, int number, string address, string name)
+        {
+
+            customerId = id;
+            customerNumber = number;
+            customerAddress = address;
+            customerName = name;
+        }
+
+        public void getCustomerInfo()
+        {
+            Console.WriteLine("Customer info");
+            Console.WriteLine($"ID: id")
+        }   
+
+    }
+
+
+    internal class Program
+    {
+        //creating customer class
+
+
+
+    }
+}
