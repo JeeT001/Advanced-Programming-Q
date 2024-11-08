8f6dc14c2f9df928d1ca675dd57900693ca676a3
Simranjit Babbar <111028218+JeeT001@users.noreply.github.com>
2024-11-08 15:47:13 +1300

    Setting up

--- bankAccountManagementApplication/obj/Debug/net8.0/bankAccountManagementApplication.AssemblyInfo.cs
+++ bankAccountManagementApplication/obj/Debug/net8.0/bankAccountManagementApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bankAccountManagementApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ab5de24fb3bfc1f6aa0fb8fe81685cb938e157c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba9762bb1ee9f622bce722e31d320b6955b5f5e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("bankAccountManagementApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bankAccountManagementApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
