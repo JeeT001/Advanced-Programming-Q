664a6fab2f9ce793f693f5d169738a887cc4b912
Simranjit Babbar <111028218+JeeT001@users.noreply.github.com>
2024-11-08 16:12:50 +1300

    Created Form app

---
+++ BankAccountManagementApp/BankAccountManagementApp/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace BankAccountManagementApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.accountTypeComboBox = new System.Windows.Forms.ComboBox();
+            this.amountTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.viewInfoButton = new System.Windows.Forms.Button();
+            this.depositButton = new System.Windows.Forms.Button();
+            this.withdrawButton = new System.Windows.Forms.Button();
+            this.calculateInterestButton = new System.Windows.Forms.Button();
+            this.transactionListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // accountTypeComboBox
+            // 
+            this.accountTypeComboBox.FormattingEnabled = true;
+            this.accountTypeComboBox.Location = new System.Drawing.Point(248, 110);
+            this.accountTypeComboBox.Name = "accountTypeComboBox";
+            this.accountTypeComboBox.Size = new System.Drawing.Size(138, 21);
+            this.accountTypeComboBox.TabIndex = 0;
+            this.accountTypeComboBox.Text = "Select Account Type";
+            // 
+            // amountTextBox
+            // 
+            this.amountTextBox.Location = new System.Drawing.Point(248, 197);
+            this.amountTextBox.Name = "amountTextBox";
+            this.amountTextBox.Size = new System.Drawing.Size(100, 20);
+            this.amountTextBox.TabIndex = 1;
+            this.amountTextBox.TextChanged += new System.EventHandler(this.amountTextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(248, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Account Type";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(248, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Amount";
+            // 
+            // viewInfoButton
+            // 
+            this.viewInfoButton.Location = new System.Drawing.Point(75, 258);
+            this.viewInfoButton.Name = "viewInfoButton";
+            this.viewInfoButton.Size = new System.Drawing.Size(75, 23);
+            this.viewInfoButton.TabIndex = 4;
+            this.viewInfoButton.Text = "View Info";
+            this.viewInfoButton.UseVisualStyleBackColor = true;
+            // 
+            // depositButton
+            // 
+            this.depositButton.Location = new System.Drawing.Point(196, 258);
+            this.depositButton.Name = "depositButton";
+            this.depositButton.Size = new System.Drawing.Size(75, 23);
+            this.depositButton.TabIndex = 5;
+            this.depositButton.Text = "Deposit";
+            this.depositButton.UseVisualStyleBackColor = true;
+            // 
+            // withdrawButton
+            // 
+            this.withdrawButton.Location = new System.Drawing.Point(322, 258);
+            this.withdrawButton.Name = "withdrawButton";
+            this.withdrawButton.Size = new System.Drawing.Size(75, 23);
+            this.withdrawButton.TabIndex = 6;
+            this.withdrawButton.Text = "Withdraw";
+            this.withdrawButton.UseVisualStyleBackColor = true;
+            // 
+            // calculateInterestButton
+            // 
+            this.calculateInterestButton.Location = new System.Drawing.Point(429, 258);
+            this.calculateInterestButton.Name = "calculateInterestButton";
+            this.calculateInterestButton.Size = new System.Drawing.Size(108, 23);
+            this.calculateInterestButton.TabIndex = 7;
+            this.calculateInterestButton.Text = "Calculate Interest";
+            this.calculateInterestButton.UseVisualStyleBackColor = true;
+            // 
+            // transactionListBox
+            // 
+            this.transactionListBox.FormattingEnabled = true;
+            this.transactionListBox.Location = new System.Drawing.Point(251, 313);
+            this.transactionListBox.Name = "transactionListBox";
+            this.transactionListBox.Size = new System.Drawing.Size(120, 95);
+            this.transactionListBox.TabIndex = 8;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 460);
+            this.Controls.Add(this.transactionListBox);
+            this.Controls.Add(this.calculateInterestButton);
+            this.Controls.Add(this.withdrawButton);
+            this.Controls.Add(this.depositButton);
+            this.Controls.Add(this.viewInfoButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.amountTextBox);
+            this.Controls.Add(this.accountTypeComboBox);
+            this.Name = "MainForm";
+            this.Text = "Bank Account Management";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox accountTypeComboBox;
+        private System.Windows.Forms.TextBox amountTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button viewInfoButton;
+        private System.Windows.Forms.Button depositButton;
+        private System.Windows.Forms.Button withdrawButton;
+        private System.Windows.Forms.Button calculateInterestButton;
+        private System.Windows.Forms.ListBox transactionListBox;
+    }
+}
+
