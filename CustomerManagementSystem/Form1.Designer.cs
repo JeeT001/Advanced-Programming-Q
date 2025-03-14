5038ab65c2f4e22124dd7f0f5dc4ea1da30d7467
Simranjit Singh Babbar <111028218+JeeT001@users.noreply.github.com>
2025-03-14 13:09:13 +1300

    Finalised

--- CustomerManagementSystem/Form1.Designer.cs
+++ CustomerManagementSystem/Form1.Designer.cs
@@ -137,7 +137,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(686, 397);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(687, 397);
             this.Controls.Add(this.lstCustomers);
             this.Controls.Add(this.txtCustomerName);
             this.Controls.Add(this.label3);
